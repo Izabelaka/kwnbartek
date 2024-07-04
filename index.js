afebe3144c75c7b1af65dec10d662de23ef0be67
Izabela Kazana <izabelaxkazana@gmail.com>
2024-07-04 17:00:17 +0200

    up

--- index.js
+++ index.js
@@ -1,72 +1,75 @@
+// mobile navigation
 const burgerBtn = document.querySelector(".burgerBtn");
 const mobileNavigation = document.querySelector(".mobileNavigation");
 const items = document.querySelector(".mobileNavigation__items");
 
 const activeNavigation = () => {
-  mobileNavigation.classList.toggle("navigationHidden");
+	mobileNavigation.classList.toggle("navigationHidden");
 
-  if (mobileNavigation.classList.contains("navigationHidden")) {
-    // Dodajemy styl 'transition', gdy menu jest aktywowane
-    items.style.transition = "opacity 1s ease 0.5s";
-    items.style.opacity = 1;
-  } else {
-    // Usuwamy styl 'transition', gdy menu jest deaktywowane
-    items.style.transition = "";
-    items.style.opacity = 0;
-  }
+	if (mobileNavigation.classList.contains("navigationHidden")) {
+		items.style.transition = "opacity 1s ease 0.5s";
+		items.style.opacity = 1;
+		document.body.classList.add("no-scroll");
+	} else {
+		items.style.transition = "";
+		items.style.opacity = 0;
+		document.body.classList.remove("no-scroll");
+	}
 };
 
 burgerBtn.addEventListener("click", activeNavigation);
 
-////////accordion start
+// end mobile navigation
+
+// start accordion
 
 const accordionBtns = document.querySelectorAll(".accordion__btn");
 
 function openAccordionTextBox() {
-  const textBox = this.nextElementSibling;
-  const iconSecond = this.querySelector(".accordion__icon__second");
+	const textBox = this.nextElementSibling;
+	const iconSecond = this.querySelector(".accordion__icon__second");
 
-  if (textBox.classList.contains("accordion__active")) {
-    textBox.classList.remove("accordion__active");
-    iconSecond.classList.remove("rotate");
-  } else {
-    closeAccordionTextBox();
-    textBox.classList.add("accordion__active");
-    iconSecond.classList.add("rotate");
-  }
+	if (textBox.classList.contains("accordion__active")) {
+		textBox.classList.remove("accordion__active");
+		iconSecond.classList.remove("rotate");
+	} else {
+		closeAccordionTextBox();
+		textBox.classList.add("accordion__active");
+		iconSecond.classList.add("rotate");
+	}
 }
 
 function closeAccordionTextBox() {
-  const allActiveTextboxes = document.querySelectorAll(
-    ".accordion__text-box.accordion__active"
-  );
-  const allActiveIcons = document.querySelectorAll(
-    ".accordion__icon__second.rotate"
-  );
+	const allActiveTextboxes = document.querySelectorAll(
+		".accordion__text-box.accordion__active"
+	);
+	const allActiveIcons = document.querySelectorAll(
+		".accordion__icon__second.rotate"
+	);
 
-  allActiveTextboxes.forEach(textBox => {
-    textBox.classList.remove("accordion__active");
-  });
+	allActiveTextboxes.forEach((textBox) => {
+		textBox.classList.remove("accordion__active");
+	});
 
-  allActiveIcons.forEach(icon => {
-    icon.classList.remove("rotate");
-  });
+	allActiveIcons.forEach((icon) => {
+		icon.classList.remove("rotate");
+	});
 }
 
-accordionBtns.forEach(btn =>
-  btn.addEventListener("click", openAccordionTextBox)
+accordionBtns.forEach((btn) =>
+	btn.addEventListener("click", openAccordionTextBox)
 );
 
-//////accordion end
+// end accordion
 
-/////////footer start
+// start footer
 const footerYear = document.querySelector(".footer__year");
 
 const handleCurrentYear = () => {
-  const year = new Date().getFullYear();
-  footerYear.innerText = year;
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
 };
 
 handleCurrentYear();
 
-/////////footer end
+// end footer
